8b1acdbe06f6cfd4eb1839c2d1321d45bf62b612
Buse Güvez <guvezbuse@gmail.com>
2023-12-04 19:22:16 +0300

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Burası yorum satırıdır.
+		//Dosya isimlerinde Türkçe karakter kullanılmamalıdır.
+		
+		System.out.println("HELLO WORLD !");
+		//Bu komut konsola mesaj yazdırır.
+
+	}
+
+}
